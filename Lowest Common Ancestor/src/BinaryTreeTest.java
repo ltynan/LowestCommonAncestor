aaadaf3f093664780030b0774c84ffc51657fc66
ltynan <tynanlu@tcd.ie>
2017-10-11 15:47:57 +0100

    Initial checkin of lowest common ancestor problem without testing.

---
+++ Lowest Common Ancestor/src/BinaryTreeTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BinaryTreeTest {
+
+	@Test
+	public void testFindLCA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindLCANode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
