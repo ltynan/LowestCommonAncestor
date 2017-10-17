acb17c93c197852222da9bd5130eeb61b5475c0e
ltynan <tynanlu@tcd.ie>
2017-10-17 16:35:01 +0100

    Both sides of Binary tree now JUnit tested

--- Lowest Common Ancestor/src/BinaryTreeTest.java
+++ Lowest Common Ancestor/src/BinaryTreeTest.java
@@ -9,22 +9,39 @@ public class BinaryTreeTest {
 	public void testLCARightSide() {
 		BinaryTree tree = new BinaryTree();
 
-		tree.root = new Node(12);							//	12
-		tree.root.left = new Node(8);					// 8
-		tree.root.right = new Node(16);								//16
-		tree.root.left.left = new Node(6);			//6		
-		tree.root.left.right = new Node(10);				//10
-		tree.root.right.left = new Node(14);					//14	
-		tree.root.right.right = new Node(18);								//18
-		
-		System.out.println("LCA(14, 18) = " +
-							tree.findLCA(14, 18).data);
+		tree.root = new Node(12);					//				 _ 12 _
+		tree.root.left = new Node(8);				// 			   / 	   \
+		tree.root.right = new Node(16);				//			_ 8 _	  _ 16 _
+		tree.root.left.left = new Node(6);			//		   /     \   /		\
+		tree.root.left.right = new Node(10);		//		  6 	 10 14		18
+		tree.root.right.left = new Node(14);		//							  \
+		tree.root.right.right = new Node(18);		//							   27
+		tree.root.right.right.right = new Node(27); //		
+		System.out.println("LCA(14, 27) = " +
+							tree.findLCA(14, 27).data);
 		int answer = 16;										// initial test on the solution implemented
-		assertEquals(answer, tree.findLCA(14, 18).data);
+		assertEquals(answer, tree.findLCA(14, 27).data);
 
 	}
 	
-	
+	@Test
+	public void testLCALeftSide() {
+		BinaryTree tree = new BinaryTree();
+
+		tree.root = new Node(12);					//				 _ 12 _
+		tree.root.left = new Node(8);				// 			   / 	   \
+		tree.root.right = new Node(16);				//			_ 8 _	  _ 16 _
+		tree.root.left.left = new Node(6);			//		   /     \   /		\
+		tree.root.left.right = new Node(10);		//		  6 	 10 14		18
+		tree.root.right.left = new Node(14);		//							  \
+		tree.root.right.right = new Node(18);		//							   27
+		tree.root.right.right.right = new Node(27); //		
+		System.out.println("LCA(6, 8) = " +
+							tree.findLCA(6, 8).data);
+		int answer = 8;								
+		assertEquals(answer, tree.findLCA(6, 8).data);
+
+	}
 	
 	@Test
 	public void testEmptyTree(){
