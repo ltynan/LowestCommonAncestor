67a83544cc3b3023f6d912ed444d6a64d92cd8d8
ltynan <tynanlu@tcd.ie>
2017-10-20 15:38:29 +0100

    LCA Testing added for differnt tree permutations

--- Lowest Common Ancestor/src/BinaryTreeTest.java
+++ Lowest Common Ancestor/src/BinaryTreeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class BinaryTreeTest {
 
 	@Test
-	public void testLCARightSide() {
+	public void testFindLCA() {
 		BinaryTree tree = new BinaryTree();
 
 		tree.root = new Node(12);					//				 _ 12 _
@@ -19,29 +19,25 @@ public class BinaryTreeTest {
 		tree.root.right.right.right = new Node(27); //		
 		System.out.println("LCA(14, 27) = " +
 							tree.findLCA(14, 27).data);
-		int answer = 16;										// initial test on the solution implemented
+		int answer = 16;										// test LCA on right side of Binary Tree
 		assertEquals(answer, tree.findLCA(14, 27).data);
-
-	}
-	
-	@Test
-	public void testLCALeftSide() {
-		BinaryTree tree = new BinaryTree();
-
-		tree.root = new Node(12);					//				 _ 12 _
-		tree.root.left = new Node(8);				// 			   / 	   \
-		tree.root.right = new Node(16);				//			_ 8 _	  _ 16 _
-		tree.root.left.left = new Node(6);			//		   /     \   /		\
-		tree.root.left.right = new Node(10);		//		  6 	 10 14		18
-		tree.root.right.left = new Node(14);		//							  \
-		tree.root.right.right = new Node(18);		//							   27
-		tree.root.right.right.right = new Node(27); //		
 		
 		System.out.println("LCA(6, 8) = " +
 							tree.findLCA(6, 8).data);
-		int answer = 8;								
-		assertEquals(answer, tree.findLCA(6, 8).data);
-
+		answer = 8;								
+		assertEquals(answer, tree.findLCA(6, 8).data);			// test LCA on left side of Binary Tree
+		
+		tree = new BinaryTree();
+		tree.root = new Node(12);					//				 _ 12 _
+		tree.root.left = new Node(8);				// 			   / 	   \
+		tree.root.right = new Node(16);				//			  8		    16 
+		
+		answer =12;
+		assertEquals(answer, tree.findLCA(12, 8).data);			// test LCA for tree with only 3 nodes
+		
+		
+		tree.root.left=null;
+		assertEquals(answer, tree.findLCA(12, 8).data);  		// test LCA for tree with only 2 nodes
 	}
 	
 	@Test
