af2010f38d6d63528b2de1d470c79d63750da159
ltynan <tynanlu@tcd.ie>
2017-11-01 17:39:53 +0000

    Additional code coverage testing added

--- Lowest Common Ancestor/src/DAGTest.java
+++ Lowest Common Ancestor/src/DAGTest.java
@@ -20,27 +20,13 @@ public class DAGTest {
 	}*/
 
 	@Test
-	public void testNullLowestCommonAncestor() {
-		
-		nodes = new ArrayList<>();
-		DAG = new DirectedAcylicGraph<Node>();
-		nodes.add(new Node(null));
-		nodes.add(new Node(1));
-		nodes.add(new Node(2));
-		DAG.setRoot(nodes.get(0));	
-		
-		assertEquals(null, DAG.lowestCommonAncestor(nodes.get(1), nodes.get(2)));
-
-	}
-	
-	@Test
 	public void testLowestCommonAncestor() {
 		nodes = new ArrayList<>();
 		DAG = new DirectedAcylicGraph<Node>();
 		for (int i = 0; i < 10; i++) {
 			nodes.add(new Node(i));
 		}
-		DAG.setRoot(nodes.get(0));
+		DAG.setRoot(nodes.get(0));	
 		nodes.get(0).addEdge(nodes.get(1));
 		nodes.get(0).addEdge(nodes.get(2));
 		nodes.get(1).addEdge(nodes.get(3));
@@ -53,9 +39,40 @@ public class DAGTest {
 		nodes.get(6).addEdge(nodes.get(7));
 		nodes.get(6).addEdge(nodes.get(9));
 		nodes.get(7).addEdge(nodes.get(9));
-		assertEquals("nodes(nodes9,nodes8) = 6", nodes.get(6), DAG.lowestCommonAncestor(nodes.get(9), nodes.get(8)));
-		assertEquals("nodes(nodes8,nodes4) = 2", nodes.get(2), DAG.lowestCommonAncestor(nodes.get(8), nodes.get(4)));
-		assertEquals("nodes(nodes8,nodes3) = 0", nodes.get(0), DAG.lowestCommonAncestor(nodes.get(8), nodes.get(3)));
+		assertEquals(nodes.get(6), DAG.lowestCommonAncestor(nodes.get(9), nodes.get(8)));
+		assertEquals(nodes.get(2), DAG.lowestCommonAncestor(nodes.get(8), nodes.get(4)));
+		assertEquals(nodes.get(0), DAG.lowestCommonAncestor(nodes.get(8), nodes.get(3)));
 
+		nodes = new ArrayList<>();
+		DAG = new DirectedAcylicGraph<Node>();
+		for (int i = 0; i < 9; i++) {
+			nodes.add(new Node(i));
+		}
+		DAG.setRoot(nodes.get(0));
+		nodes.get(0).addEdge(nodes.get(1));
+		nodes.get(0).addEdge(nodes.get(2));
+		nodes.get(1).addEdge(nodes.get(4));
+		nodes.get(1).addEdge(nodes.get(5));
+		nodes.get(2).addEdge(nodes.get(3));
+		nodes.get(3).addEdge(nodes.get(5));
+		nodes.get(3).addEdge(nodes.get(6));
+		nodes.get(5).addEdge(nodes.get(7));
+		nodes.get(5).addEdge(nodes.get(8));
+		
+		assertEquals(nodes.get(0).data, DAG.lowestCommonAncestor(nodes.get(1), nodes.get(2)).data);
+		assertEquals(nodes.get(3).data, DAG.lowestCommonAncestor(nodes.get(8), nodes.get(6)).data);
+		assertEquals(nodes.get(3).data, DAG.lowestCommonAncestor(nodes.get(8), nodes.get(5)).data);
+	}
+	
+	@Test
+	public void testNullLowestCommonAncestor() {
+		nodes = new ArrayList<>();
+		DAG = new DirectedAcylicGraph<Node>();
+		nodes.add(new Node(null));
+		nodes.add(new Node(1));
+		nodes.add(new Node(2));
+		DAG.setRoot(nodes.get(0));	
+
+		assertEquals(null, DAG.lowestCommonAncestor(nodes.get(1), nodes.get(2)));
 	}
 }
